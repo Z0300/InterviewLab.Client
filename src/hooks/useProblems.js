230385d7db0f76c99db2f5178b4a1f9e48b295a5
subZero <marvinfordan08@gmail.com>
2025-11-24 13:26:06 +0800

    add problem feature

--- src/hooks/useProblems.js
+++ src/hooks/useProblems.js
@@ -2,11 +2,13 @@ import { useQuery } from "@tanstack/react-query";
 import api from "../api.js";
 
 const useProblems = () => {
-  const fetchProblems = () => api.get("/problems").then((res) => res.data.data);
-
   return useQuery({
     queryKey: ["problems"],
-    queryFn: fetchProblems,
+    queryFn: async () =>
+      await api
+        .get("/problems")
+        .then((res) => res.data.data)
+        .catch((err) => err.data.errors),
     staleTime: 10 * 1000,
   });
 };
---
+++ src/pages/admin/NewProblem.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import ProblemForm from "../../components/ProblemForm.jsx";
+
+const NewProblem = () => {
+  return <ProblemForm titleHeading="Create Problem" />;
+};
+export default NewProblem;
